bf08d5b599f2b222527d364dc6a206146567ff99
Yehonatan Daniv <maggotfish@gmail.com>
2026-04-09 20:38:39 +0300

    Interact split types (#181)
    * Create plan
    
    * Revisited types for easier documentation and maintainability

---
+++ packages/interact/src/types/controller.ts
@@ -0,0 +1,34 @@
+import type { StateParams } from './triggers';
+
+export interface IInteractionController {
+  element: HTMLElement;
+  key: string | undefined;
+  connected: boolean;
+  sheet: CSSStyleSheet | null;
+  useFirstChild: boolean;
+  _observers: WeakMap<HTMLElement, MutationObserver>;
+  connect(key?: string): void;
+  disconnect(options?: { removeFromCache?: boolean }): void;
+  update(): void;
+  toggleEffect(
+    effectId: string,
+    method: StateParams['method'],
+    item?: HTMLElement | null,
+    isLegacy?: boolean,
+  ): void;
+  getActiveEffects(): string[];
+  renderStyle(cssRules: string[]): void;
+  watchChildList(listContainer: string): void;
+  _childListChangeHandler(listContainer: string, entries: MutationRecord[]): void;
+}
+
+export interface IInteractElement extends HTMLElement {
+  _internals: (ElementInternals & { states: Set<string> }) | null;
+  controller: IInteractionController;
+  connectedCallback(): void;
+  disconnectedCallback(): void;
+  connect(key?: string): void;
+  disconnect(options?: { removeFromCache?: boolean }): void;
+  toggleEffect(effectId: string, method: StateParams['method'], item?: HTMLElement | null): void;
+  getActiveEffects(): string[];
+}
