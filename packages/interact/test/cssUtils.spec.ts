1b21bd3078516c0f7bfa491bb472d79f9d03edf9
Ameer Abu-Fraiha <ameerf@wix.com>
2026-06-25 20:06:12 +0300

    fixing entrance SSR - enter='done' (#243)
    * fixing entrance SSR - enter='done'
    
    * format
    
    * rule separation
    
    * oopsy

--- packages/interact/test/cssUtils.spec.ts
+++ packages/interact/test/cssUtils.spec.ts
@@ -207,13 +207,14 @@ describe('CSSRuleToString', () => {
     expect(CSSRuleToString(rule)).toEqual(expected);
   });
 
-  it('should add :not([data-interact-enter]) when addInitialSelector is true', () => {
+  it('should dataInteractEnterSelector when provided', () => {
     const rule: CSSRuleData = {
       key: 'my-el',
-      addInitialSelector: true,
+      dataInteractEnterSelector: ':not([data-interact-enter="done"])',
       declarations: [{ name: 'opacity', value: '0' }],
     };
-    const expected = '[data-interact-key="my-el"]:not([data-interact-enter]) {\nopacity: 0;\n}';
+    const expected =
+      '[data-interact-key="my-el"]:not([data-interact-enter="done"]) {\nopacity: 0;\n}';
     expect(CSSRuleToString(rule)).toEqual(expected);
   });
 
@@ -253,7 +254,7 @@ describe('CSSRuleToString', () => {
     const rule: CSSRuleData = {
       key: 'my-el',
       childSelector: '.child',
-      addInitialSelector: true,
+      dataInteractEnterSelector: ':not([data-interact-enter="done"])',
       states: ['hover'],
       media: '(min-width: 1024px)',
       declarations: [
@@ -262,7 +263,7 @@ describe('CSSRuleToString', () => {
       ],
     };
     const expected =
-      '@media (min-width: 1024px) {\n[data-interact-key="my-el"]:is(:state(hover), :--hover, [data-interact-effect~="hover"]) .child:not([data-interact-enter]) {\nopacity: 1;\ncolor: blue;\n}\n}';
+      '@media (min-width: 1024px) {\n[data-interact-key="my-el"]:is(:state(hover), :--hover, [data-interact-effect~="hover"]) .child:not([data-interact-enter="done"]) {\nopacity: 1;\ncolor: blue;\n}\n}';
     expect(CSSRuleToString(rule)).toEqual(expected);
   });
 });
